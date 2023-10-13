1c49b04ae6ffb282e847328b6f22365c9b48aa98
St-H123 <hazels.05.remount@icloud.com>
2023-10-13 21:07:35 +0200

    fixed non branch not grey when root

---
+++ similar-to-bash.omp.json-installer.sh
@@ -0,0 +1,10 @@
+##
+sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
+sudo chmod +x /usr/local/bin/oh-my-posh
+
+## Download the themes
+mkdir ~/.poshthemes
+wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/themes.zip -O ~/.poshthemes/themes.zip
+unzip ~/.poshthemes/themes.zip -d ~/.poshthemes
+chmod u+rw ~/.poshthemes/*.json
+rm ~/.poshthemes/themes.zip
